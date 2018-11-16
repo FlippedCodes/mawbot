b41e36715b86c77f63448023b138d5836468e6b3
FlipperLP <philip.schaefer0@gmail.com>
2018-11-16 23:35:02 +0100

    fixed avatar color

--- commands/avatar.js
+++ commands/avatar.js
@@ -1,24 +1,25 @@
 module.exports.run = async (client, message, args, con, config) => {
   // id not working
-  const target = message.mentions.users.first() || message.guild.members.get(args[0]) || message.author;
+  // const target = message.mentions.users.first() || message.guild.members.get(args[0]) || message.author;
+  const target = message.mentions.members.first() || message.guild.members.get(args[0]) || message.member;
 
   let user_color = 6447714;
   if (target.lastMessage) user_color = target.lastMessage.member.displayColor;
 
   const embed = {
-    title: target.tag,
-    url: target.avatarURL,
-    color: user_color,
+    title: target.user.tag,
+    url: target.user.displayAvatarURL,
+    color: target.colorRole.color,
     timestamp: new Date(),
     image: {
-      url: target.avatarURL,
+      url: target.user.displayAvatarURL,
     },
     footer: {
       icon_url: target.client.user.displayAvatarURL,
       text: target.client.user.tag,
     },
   };
-  message.channel.send(`<${target.avatarURL}>`);
+  message.channel.send(`<${target.user.displayAvatarURL}>`);
   await message.channel.send({ embed });
 };
 
