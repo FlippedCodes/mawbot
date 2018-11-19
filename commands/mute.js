dfb4553d518e9c7eaf32b7a81fe758a73487ef49
FlipperLP <philip.schaefer0@gmail.com>
2018-11-19 23:09:30 +0100

    split up code for better understanding

--- commands/mute.js
+++ commands/mute.js
@@ -1,7 +1,11 @@
 const client = module.require('discord.js');
 
 module.exports.run = async (client, message, args, con, config) => {
-  if (!message.member.roles.find('name', config.teamRole)) return message.channel.send(`Do I know you **${message.author.tag}**? Only the Devs can use this~`).then(message.react('❌'));
+  if (!message.member.roles.find('name', config.teamRole)) {
+    message.channel.send(`Do I know you **${message.author.tag}**? Only the Devs can use this~`)
+      .then(message.react('❌'));
+    return;
+  }
 
   const member = await message.mentions.members.first() || message.guild.members.get(args[0]);
 
