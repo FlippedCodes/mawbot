699950bf8e9fdcad2a526303881b66c1d1373624
FlipperLP <philip.schaefer0@gmail.com>
2018-11-29 18:43:38 +0100

    moved to invalid_cmd function

--- commands/rp-new.js
+++ commands/rp-new.js
@@ -128,21 +128,8 @@ module.exports.run = async (client, message, args, con, config) => {
       // return;
 
     default:
-      const confusResponses = [
-        'You... what, now?',
-        'Pardon me?',
-        'Hah, yeah... what?',
-        `Sorry, I have no idea what \`${subcmd}\` means.`,
-        'Come again?',
-        'Maybe you should try something else there, buddy.',
-        `I tried to understand \`${subcmd}\`, trust me, but I just cannot.`,
-        `💩 Invalid comamnd \`${subcmd}\``,
-        `Sorry, I don't know this command -  \`${subcmd}\``,
-        `Eh? Do you speak my language? Because I don't know \`${subcmd}\`...`,
-      ];
-
-      const randomChoice = Math.floor(Math.random() * confusResponses.length);
-      message.channel.send(confusResponses[randomChoice]);
+      client.functions.get('invalid_cmd').run(message, subcmd)
+        .catch(console.log);
       return;
   }
 };
