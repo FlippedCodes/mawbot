b454a8d298cd75c6fa090663008dbcd98e510815
FlipperLP <philip.schaefer0@gmail.com>
2018-12-21 18:20:55 +0100

    fixed spelling and changed reaction icon

--- functions/usersetup_rp_channel.js
+++ functions/usersetup_rp_channel.js
@@ -1,9 +1,9 @@
 module.exports.run = async (channel, message) => {
-  channel.send(`<@${message.author.id}> \nYour rp channel is now created! Please use \`=rp channelinfo\` for more information and the currenty status of your chatroom`);
-  channel.send('This is the setup guide for your channel, please click the reactions to the Questions.');
-  channel.send('Do you want a private room or a room where everyone can write in?')
+  channel.send(`<@${message.author.id}> \nYour rp channel is now created! Please use \`=rp info\` for more information and the currenty status of your chatroom`);
+  channel.send('This is the setup guide for your channel, please click the reactions to the questions.');
+  channel.send('Do you want a private room or a public room where everyone can write in?')
     .then((msg) => {
-      msg.react('🔑');
+      msg.react('🚪');
       msg.react('🔓');
     });
   channel.send('What type of room would you prefer? Make sure you have the propper roles form \n🔅  SFW\n🔞  NSFW\n☠  NSFL')
