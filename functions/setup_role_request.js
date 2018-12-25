3709133947917b4fc4e645e1f5a153ebf065017c
FlipperLP <philip.schaefer0@gmail.com>
2018-12-25 14:05:13 +0100

    renewed rolerequest

--- functions/setup_role_request.js
+++ functions/setup_role_request.js
@@ -3,7 +3,44 @@ module.exports.run = async (client, servers, config) => {
   [servers.rolerequest_main, servers.rolerequest_testing].forEach((server) => {
     if (!client.channels.get(server)) return;
     client.channels.get(server).bulkDelete(10);
-    client.channels.get(server).send(`Click on the reactions to get the roles!\nPlease read <#${config.info_channelID}> for a more details about the roles!\n\n:one: Prey\n\n:two: Prey/Pred\n\n:three: Pred\n\n:four: No NSFW\n\n:five: NSFL Access`)
+    client.channels.get(server).send({
+      embed: {
+        color: 16777215,
+        title: 'Rolerequest',
+        description: `Click on the reactions to get the roles!\nPlease read <#${config.info_channelID}> for a more details about the roles!`,
+        fields: [{
+          name: 'Prey',
+          value: ':one:',
+          inline: true,
+        },
+        {
+          name: 'Prey/Pred',
+          value: ':two:',
+          inline: true,
+        },
+        {
+          name: 'Pred',
+          value: ':three:',
+          inline: true,
+        },
+        {
+          name: 'No NSFW',
+          value: ':four:',
+          inline: true,
+        },
+        {
+          name: 'NSFL Access',
+          value: ':five:',
+          inline: true,
+        },
+        ],
+        timestamp: new Date(),
+        footer: {
+          icon_url: client.user.displayAvatarURL,
+          text: client.user.tag,
+        },
+      },
+    })
       .then(async (message) => {
         await message.react('1⃣');
         await message.react('2⃣');
