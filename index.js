d6bc8f497d7c63caf23497bbfa619f6f2b2b11a8
FlipperLP <philip.schaefer0@gmail.com>
2019-02-06 20:06:41 +0100

    edit followwarnings fixed

--- index.js
+++ index.js
@@ -195,7 +195,10 @@ client.on('message', async (message) => {
 
   con.query(`SELECT * FROM rp_timer WHERE id = '${message.channel.id}' AND archived = 'f'`, async (err, rows) => {
     if (err) throw err;
-    if (rows[0]) con.query(`UPDATE rp_timer SET timeLeft = '${servers.RPChannelTime}' WHERE id = '${message.channel.id}'`);
+    if (rows[0]) {
+      con.query(`UPDATE rp_timer SET timeLeft = '${servers.RPChannelTime}' WHERE id = '${message.channel.id}'`);
+      con.query(`UPDATE rp_timer SET warned = 'f' WHERE id = '${message.channel.id}' AND warned = 't'`);
+    }
   });
 
   let config;
