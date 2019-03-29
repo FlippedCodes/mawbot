eb6657490c1b56d383e3e422c34ddead8e08913a
Phil <philip.schaefer0@gmail.com>
2019-03-29 23:54:38 +0100

    fixing totime problem

--- commands/rp.js
+++ commands/rp.js
@@ -190,7 +190,7 @@ module.exports.run = async (client, message, args, con, config) => {
         con.query(`SELECT * FROM rp_timer WHERE id = '${message.channel.id}'`, (err, rows) => {
           let time;
           if (rows[0]) {
-            time = toTime(rows[0].timeLeft, 10);
+            time = toTime(rows[0].timeLeft);
           } else {
             time = 'no time specified';
           }
