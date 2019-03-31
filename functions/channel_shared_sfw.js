ebc2edb8a4f90d8044f7fb8e8cfeac532c142ad4
Phil <philip.schaefer0@gmail.com>
2019-03-31 22:07:08 +0200

    code cleanup

--- functions/channel_shared_sfw.js
+++ functions/channel_shared_sfw.js
@@ -1,5 +1,4 @@
 module.exports.run = async (client, message, con) => {
-  let content = `\n${message.content}`;
   // let blacklisted = false;
 
   // no pic fallback
@@ -36,51 +35,6 @@ module.exports.run = async (client, message, con) => {
   //     blacklisted = true;
   //   }
   // });
-
-  // serverlink cant be shared over webhook
-  // let code;
-  // if (message.channel.id === servers.sharedChannel_main) code = servers.invite_main;
-  // if (message.channel.id === servers.sharedChannel_testing) code = servers.invite_testing;
-  // if (message.channel.id === servers.sharedChannel_night_dragon) code = servers.invite_night_dragon;
-  // if (message.channel.id === servers.sharedChannel_voretv) code = servers.invite_voretv;
-
-  // using webhook - no embet (backup)
-  // const embed = {
-  //   // description: `\`\`\`${message.content}\`\`\``,
-  //   description: content,
-  //   title: `Server: ${message.guild.name}`,
-  //   url: `https://discord.gg/${code}`,
-  //   color: message.member.displayColor,
-  //   timestamp: message.createdAt,
-  //   // footer: {
-  //   //   text: client.name,
-  //   // icon_ur": 'https://cdn.discordapp.com/embed/avatars/0.png',
-  //   // },
-  //   author: {
-  //     name: `${message.author.username} | ${message.author.tag}`,
-  //     url: pic,
-  //   },
-  //   thumbnail: {
-  //     url: pic,
-  //   },
-  //   // author: {
-  //   //   name: `${message.author.username} | ${message.author.tag}`,
-  //   //   url: message.author.avatarURL,
-  //   // },
-  // };
-  // // if (blacklisted === true) {
-  // //   message.channel.send({ embed });
-  // // } else {
-  // [servers.sharedChannel_main, servers.sharedChannel_testing, servers.sharedChannel_night_dragon, servers.sharedChannel_voretv].forEach((server) => {
-  //   if (client.channels.exists('id', server)) {
-  //     client.channels.get(server).send({ embed })
-  //       .catch(console.error);
-  //   } else {
-  //     message.reply(`Sorry, but the Server with the ID **${server}** seems not to be available. Please report this to the team or directly to the Developers!`);
-  //   }
-  // });
-  // // }
-  // await message.delete();
 };
 
 module.exports.help = {
