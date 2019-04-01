35782ea976d3763c06e976bf71ed7150f3bed37c
Phil <philip.schaefer0@gmail.com>
2019-04-01 22:13:15 +0200

    exchanged webhook with embed, to see if its better

--- functions/channel_shared_sfw.js
+++ functions/channel_shared_sfw.js
@@ -1,3 +1,5 @@
+const Discord = require('discord.js');
+
 module.exports.run = async (client, message, con) => {
   // let blacklisted = false;
 
@@ -10,21 +12,33 @@ module.exports.run = async (client, message, con) => {
       const vorenetwork_channel = client.channels.find(channel => channel.id === ID.channelID);
       if (vorenetwork_channel.id !== message.channel.id) {
         con.query(`SELECT * FROM shared_channels WHERE channelID = '${message.channel.id}'`, async (err, rows) => {
-          vorenetwork_channel.fetchWebhooks()
-            .then((webhook) => {
-              const hook = webhook.find(hook => hook.name === rows[0].webhookName);
-              hook.send(`[${message.channel.guild.name}]\n${message.content}`, {
-                // FIXME: name too long for discord api; move servname to end of message
-                // username: `${message.author.username} [${message.channel.guild.name}]`,
-                username: message.author.username,
-                avatarURL: pic,
-              })
-                .catch((error) => {
-                  console.log(error);
-                  message.channel.send('Something went wrong sending the message to one of the other servers. Please report this to the Team.');
-                  return;
-                });
+          const embed = new Discord.RichEmbed()
+            .setAuthor(message.author.username)
+            .setColor(message.member.displayColor)
+            .setDescription(message.content)
+            .setThumbnail(pic)
+            .setTimestamp();
+          vorenetwork_channel.send({ embed })
+            .catch((error) => {
+              console.log(error);
+              message.channel.send('Something went wrong sending the message to one of the other servers. Please report this to the Team.');
+              return;
             });
+          // vorenetwork_channel.fetchWebhooks()
+          //   .then((webhook) => {
+          //     const hook = webhook.find(hook => hook.name === rows[0].webhookName);
+          //     hook.send(`[${message.channel.guild.name}]\n${message.content}`, {
+          //       // FIXME: name too long for discord api; move servname to end of message
+          //       // username: `${message.author.username} [${message.channel.guild.name}]`,
+          //       username: message.author.username,
+          //       avatarURL: pic,
+          //     })
+          //       .catch((error) => {
+          //         console.log(error);
+          //         message.channel.send('Something went wrong sending the message to one of the other servers. Please report this to the Team.');
+          //         return;
+          //       });
+          //   });
         });
       }
     });
