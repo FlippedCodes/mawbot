e21f979b09a469ceb1fd7f88331c77de935f9e56
Phil <philip.schaefer0@gmail.com>
2019-04-06 22:30:52 +0200

    activated af_api

---
+++ functions/fa_api_bot.js
@@ -0,0 +1,124 @@
+const { Recent, Type, Login } = require('furaffinity');
+
+const Discord = require('discord.js');
+
+const client = new Discord.Client({ disableEveryone: true });
+
+const config = require('../config/furaffinity/config.json');
+
+module.exports.run = async (fs) => {
+  // login
+  let token;
+  let clientID;
+  if (fs.existsSync('./config/test_token.json')) {
+    token = require('../config/test_token.json');
+    client.login(token.test_token_fa);
+    clientID = config.clientIDTesting;
+    Login(token.fa_cookie_a, token.fa_cookie_b);
+  } else {
+    client.login(process.env.BOT_TOKEN_FA);
+    clientID = config.clientID;
+    Login(process.env.FA_COOKIE_A, process.env.FA_COOKIE_B);
+  }
+
+  client.on('ready', async () => {
+    console.log(`[FurAffinity-API] Logged in as ${client.user.tag}!`);
+
+    client.user.setActivity('with \'?fa\' command');
+  });
+
+  client.on('message', async (message) => {
+    if (message.author.bot) return;
+    if (message.content.indexOf(config.prefix) !== 0) return;
+    // fix and implement own prefix || message.mentions.members.first().id !== config.clientID || message.mentions.members.first().id !== config.clientIDTesting
+
+    const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
+    const command = args.shift().toLowerCase();
+
+    switch (command) {
+      case 'fa':
+        Recent(Type.Artwork).then((res) => {
+          res[0].getSubmission().then((submission) => {
+          // submission is a Submission
+            message.channel.send('hi');
+          });
+        });
+
+        //   // get user args
+        //   const keywords = args.join(' ');
+
+        //   // testcmd
+        //   // message.channel.send(keywords);
+
+        //   // check args
+        //   // nothing here yet
+
+        //   // cap max pics
+        //   const limit = 1;
+
+        //   // loop limit
+        //   // not done yet
+
+        // command get info
+        // furaffinity.search(keywords, limit).then((fa) => {
+        //   // output
+        //   if (fa[0]) {
+        //     let image;
+        //     let i;
+        //     for (i = 0; i < 16; i++) {
+        //       if (fa[0].src !== fa[0].src.replace(`@${i}00`, '@1600')) {
+        //         image = fa[0].src.replace(`@${i}00`, '@1600');
+        //         break;
+        //       }
+        //     }
+        //     const embed = {
+        //       title: fa[0].title,
+        //       url: fa[0].url,
+        //       color: message.channel.guild.members.get(clientID).displayColor,
+        //       timestamp: new Date(),
+        //       footer: {
+        //         icon_url: client.user.displayAvatarURL,
+        //         text: client.user.username,
+        //       },
+        //       image: {
+        //         url: image,
+        //       },
+        //       author: {
+        //         name: fa[0].author.name,
+        //         url: fa[0].author.url,
+        //       },
+        //     };
+        //     message.channel.send({ embed });
+        //   } else {
+        //     message.channel.send('Sorry there are no pictures with these keywords.');
+        //   }
+        // }).catch(err => console.log(err));
+        // loop end
+        // not here yet
+        return;
+      case 'about':
+        fs.readFile('./config/furaffinity/about.txt', 'utf8', (err, data) => {
+          if (err) {
+            console.log(err);
+            message.react('❌');
+            return;
+          }
+          message.channel.send(data);
+        });
+        return;
+      case 'test':
+        Recent(Type.Artwork).then((res) => {
+          res[0].getSubmission().then((submission) => {
+            console.log('wok');
+          });
+        });
+        return;
+      default:
+        return;
+    }
+  });
+};
+
+module.exports.help = {
+  name: 'fa_api_bot',
+};
