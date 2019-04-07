6d3718f249c421cd0acf200bf321c406b19d82d9
Phil <philip.schaefer0@gmail.com>
2019-04-07 15:02:16 +0200

    datelet yandex test

--- commands/yandex.js
+++
@@ -1,9 +0,0 @@
-const yapic = require("yandex-pictures");
-
-module.exports.run = async (client, message, args, con, config) => {
-  message.channel.send(message.author.username);
-};
-
-module.exports.help = {
-  name: 'template',
-};
