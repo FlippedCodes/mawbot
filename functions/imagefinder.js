252a7eba032b65bc02160800f7a975129e31f34f
Phil <philip.schaefer0@gmail.com>
2019-04-07 21:21:44 +0200

    publisher only exception

--- functions/imagefinder.js
+++ functions/imagefinder.js
@@ -8,9 +8,13 @@ module.exports.run = async (client, con, reaction, user, message, image) => {
   reaction.remove(user);
   con.query(`SELECT * FROM image_channel WHERE channelID = '${message.channel.id}'`, async (err, rows) => {
     if (rows[0]) {
+      if (reaction.author.id !== message.author.id) {
+        reaction.remove(user);
+        return message.channel.send('Sorry, you are not the publisher of this picture.');
+      }
       message.react(client.guilds.get('451833819910373406').emojis.get('564375243662163968')).then((reaction_loading) => {
         const url = `http://iqdb.harry.lu/?url=${image}`;
-    
+
         rp(url)
           .then((html) => {
             if ($('tbody > tr > th', html)[1].firstChild.data === 'Probable match:') {
@@ -22,15 +26,15 @@ module.exports.run = async (client, con, reaction, user, message, image) => {
                 }
               }
               const link = $('tr > td > a', html)[0].attribs.href;
-              const img_id = $('tr > td > a > img', html)[0].attribs.src;
-              const pic = img_id.replace('e621/', 'https://static1.e621.net/data/');
+              // const img_id = $('tr > td > a > img', html)[0].attribs.src;
+              // const pic = img_id.replace('e621/', 'https://static1.e621.net/data/');
               const similarity = $('tbody > tr', html)[7].firstChild.firstChild.data;
               let embed = new RichEmbed()
                 .setAuthor(raiting)
                 .setTitle(`Probable match: ${similarity}`)
                 .setColor(message.member.displayColor)
                 .addField('e621 link:', link)
-                .setImage(pic)
+                // .setImage(pic)
                 .setFooter(client.user.tag, client.user.displayAvatarURL)
                 .setTimestamp();
               message.channel.send({ embed });
