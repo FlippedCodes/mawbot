6bf45e4d90199e47644e7519104444c95d9e7a84
Phil <philip.schaefer0@gmail.com>
2019-04-11 22:38:19 +0200

    added auto remove roles for nonsfw

--- commands/nsfw_forbidden.js
+++ commands/nsfw_forbidden.js
@@ -4,10 +4,13 @@ module.exports.run = async (client, message, args, con, config) => {
   const target = message.mentions.members.first() || message.guild.members.get(args[0]);
   con.query(`SELECT * FROM custom_roles_ban WHERE id = '${target.id}'`, (err, rows) => {
     if (err) throw err;
-
     if (rows.length < 1) {
       con.query(`INSERT INTO custom_roles_ban (id) VALUE ('${target.id}')`);
-      message.channel.send(`You added \`${target.user.tag}\` to the blacklist and isnt allowed to assign no_nsfw or nsfl_access anymore.\nYou should remove any access to this channels by hand`);
+      message.channel.send(`You added \`${target.user.tag}\` to the blacklist and isn't allowed to assign no_nsfw or nsfl_access anymore.`);
+      target.removeRoles(config.NSFL)
+        .catch();
+      target.addRole(config.noNSFW)
+        .catch();
     } else {
       message.channel.send(`\`${target.user.tag}\` is already on this blacklist`);
       message.react('❌');
