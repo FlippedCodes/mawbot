1b2d784f1a5d6092654c76785cfa0c326a4b64b3
Phil <philip.schaefer0@gmail.com>
2019-04-14 22:21:24 +0200

    added e621 command

--- functions/fa_api_bot.js
+++ functions/fa_api_bot.js
@@ -1,12 +1,28 @@
 const furaffinity = require('furaffinity');
 
+const rp = require('request-promise');
+
+const $ = require('cheerio');
+
 const Discord = require('discord.js');
+
+const { RichEmbed } = require('discord.js');
 
 const client = new Discord.Client({ disableEveryone: true });
 
 const config = require('../config/furaffinity/config.json');
 
-module.exports.run = async (fs) => {
+const usedRecently = new Set();
+
+function timeout(id) {
+  usedRecently.add(id);
+  setTimeout(() => {
+    usedRecently.delete(id);
+  }, 5000);
+  // 5sey timeout
+}
+
+module.exports.run = async (fs, functions) => {
   // login
   let token;
   let clientID;
@@ -31,29 +47,99 @@ module.exports.run = async (fs) => {
     if (message.author.bot) return;
     if (message.content.indexOf(config.prefix) !== 0) return;
     // TODO: implement own prefix
-    // FIXME: message.mentions.members.first().id !== config.clientID || message.mentions.members.first().id !== config.clientIDTesting
 
+    if (usedRecently.has(message.author.id)) return message.reply('sowwy, but you can\'t use me this often. Plewse wait 5 secounds between commands.');
     const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
     const command = args.shift().toLowerCase();
 
     switch (command) {
+      case 'e6':
+        message.react(client.guilds.get(config.emojiServer).emojis.get(config.loadingEmoji)).then((reaction_loading) => {
+          let [limit] = args;
+          let tags = args
+            .join(' ')
+            .slice(limit.length + 1)
+            .replace(', ', ' ');
+          if (isNaN(limit) || limit === 0) {
+            functions.get('invalid_cmd').run(message, limit);
+            reaction_loading.remove(client.user);
+            return;
+          }
+          let uri = 'https://e621.net/post/index.json';
+          if (message.channel.nsfw === false) uri = 'https://e926.net/post/index.json';
+          if (limit > 10) {
+            message.reply('you can only requwest a maximum of 10 images at the twime.');
+            reaction_loading.remove(client.user);
+            return;
+          }
+          timeout(message.author.id);
+          if (limit > 3) message.reply('you requwested over 3 images and this might take somwe time. Pleawse don\'t rush me. >.<');
+          let request = {
+            method: 'POST',
+            uri,
+            body: {
+              limit: '300',
+              tags,
+            },
+            headers: { 'User-Agent': 'Discordbot - Mawbot' },
+            json: true,
+          };
+          rp(request)
+            .then((json) => {
+              for (let i = 0; i < limit; i++) {
+                const randomChoice = Math.floor(Math.random() * json.length);
+                let typePic = 'Preview';
+                let picURL = json[randomChoice].sample_url;
+                let arrow = '🔽';
+                const extantion = json[randomChoice].file_ext;
+                if (extantion === 'gif' || extantion === 'webm') {
+                  typePic = 'Full Picture';
+                  picURL = json[randomChoice].file_url;
+                  if (extantion === 'webm') arrow = json[randomChoice].file_url;
+                }
+                let embed = new RichEmbed()
+                  .setAuthor(`Main Artist: ${json[randomChoice].artist[0]}`)
+                  .setColor(message.member.displayColor)
+                  .setTitle('E621 Link')
+                  .setURL(`https://e621.net/post/show/${json[randomChoice].id}`)
+                  .addField(typePic, arrow)
+                  .setImage(picURL)
+                  .setFooter(client.user.tag, client.user.displayAvatarURL)
+                  .setTimestamp();
+                message.channel.send({ embed })
+                  .then((msg) => {
+                    msg.react('↔')
+                      .then(() => msg.react('❌'));
+                  });
+              }
+            })
+            .then(() => {
+              reaction_loading.remove(client.user);
+              message.react('✅');
+            });
+        })
+          .catch((err) => {
+            message.channel.send('Sowwy, but it seems like something went wrong... Pleawse report this to my creator. uwu');
+            console.error(err);
+          });
+        return;
       case 'fa':
         if (args.length <= 0) return message.channel.send('Sorry, I couldn\'t find anything, because you didn\'t provide any tags. :(');
 
         // get user args
         const keywords = args.join(' ');
 
-          // testcmd
-          // message.channel.send(keywords);
+        // testcmd
+        // message.channel.send(keywords);
 
-          // check args
-          // nothing here yet
+        // check args
+        // nothing here yet
 
-          // cap max pics
-          const limit = 1;
+        // cap max pics
+        const limit = 1;
 
-          // loop limit
-          // not done yet
+        // loop limit
+        // not done yet
 
         // command get info
         furaffinity.search(keywords, limit).then((fa) => {
@@ -91,6 +177,7 @@ module.exports.run = async (fs) => {
         }).catch(err => console.log(err));
         // loop end
         // not here yet
+        timeout(message.author.id);
         return;
       case 'about':
         fs.readFile('./config/furaffinity/about.txt', 'utf8', (err, data) => {
@@ -101,13 +188,66 @@ module.exports.run = async (fs) => {
           }
           message.channel.send(data);
         });
+        // timeout(message.author.id);
+        timeout(message.author.id);
         return;
-      case 'test':
-        Recent(Type.Artwork).then((res) => {
-          res[0].getSubmission().then((submission) => {
-            console.log('wok');
+      default:
+        return;
+    }
+  });
+
+  client.on('messageReactionAdd', async (reaction, user) => {
+    // TODO: image deletion if inapropiete
+    if (user.bot) return;
+    switch (reaction.emoji.name) {
+      case '↔':
+        const id = reaction.message.embeds[0].url.replace('https://e621.net/post/show/', '');
+        const color = reaction.message.embeds[0].color;
+        let e621_id = {
+          method: 'POST',
+          uri: 'https://e621.net/post/show.json',
+          body: {
+            id,
+          },
+          headers: {
+            'User-Agent': 'Discordbot - Mawbot',
+          },
+          json: true,
+        };
+        rp(e621_id)
+          .then((json) => {
+            let source = 'none';
+            let typeSources = 'Sources';
+            if (json.sources) {
+              source = json.sources.join('\n');
+              if (json.sources.length === 1) typeSources = 'Source';
+            }
+            let rating = 'NSFW';
+            if (json.rating === 's') rating = 'NSFW';
+            let artists = json.artist.join(', ');
+            let typeArtists = 'All artists';
+            if (json.artist.length === 1) typeArtists = 'Artist';
+            let embed = new RichEmbed()
+              .setAuthor(`${typeArtists}: ${json.artist[0]}`)
+              .setColor(color)
+              .setTitle('E621 Link')
+              .setURL(`https://e621.net/post/show/${json.id}`)
+              .setDescription(`**Tags:** ${json.tags}`)
+              .addField('Rating', rating, true)
+              .addField('Score', json.score, true)
+              .addField('ID', json.id, true)
+              .addField(typeSources, source)
+              .addField('Full Picture link', json.file_url)
+              .addField('Full Picture', '🔽')
+              .setImage(json.file_url)
+              .setFooter(client.user.tag, client.user.displayAvatarURL)
+              .setTimestamp();
+            reaction.message.edit({ embed });
           });
-        });
+        return;
+      case '❌':
+        if (!reaction.me) return;
+        reaction.message.delete();
         return;
       default:
         return;
