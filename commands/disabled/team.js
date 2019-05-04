313b121a05107eef2d4a197846918f1c767adca1
Phil <philip.schaefer0@gmail.com>
2019-05-04 11:34:08 +0200

    removed unnessesary dependency

--- commands/disabled/team.js
+++ commands/disabled/team.js
@@ -1,5 +1,3 @@
-const client = module.require('discord.js');
-
 module.exports.run = async (client, message, args, con, config) => {
   const teamlist = message.guild.roles.get(config.team).members.map(s => s.presence.status).sort().join('\n');
   if (teamlist.indexOf('online' || 'dnd') >= 0) return message.reply('teammember(s) are **online**') && message.channel.send(teamlist);
