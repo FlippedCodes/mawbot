34d460f57339eabe122b580535652d36d6873b82
Phil <philip.schaefer0@gmail.com>
2019-05-05 20:19:19 +0200

    disabled old cmds

--- commands/disabled/nsfw_forbidden.js
+++ commands/disabled/nsfw_forbidden.js
@@ -1,18 +1,20 @@
+const toTime = require('pretty-ms');
+
 module.exports.run = async (client, message, args, con, config) => {
   if (!message.member.roles.find(role => role.name === config.teamRole)) return message.channel.send(`Do I know you **${message.author.tag}**? Only the **teammembers** can use this~`).then(message.react('❌'));
 
   const target = message.mentions.members.first() || message.guild.members.get(args[0]);
-  con.query(`SELECT * FROM custom_roles_underage WHERE id = '${target.id}'`, (err, rows) => {
+  con.query(`SELECT * FROM custom_roles_userage WHERE userID = '${target.id}'`, (err, rows) => {
     if (err) throw err;
     if (rows.length < 1) {
-      con.query(`INSERT INTO custom_roles_underage (id) VALUE ('${target.id}')`);
       message.channel.send(`You added \`${target.user.tag}\` to the blacklist and isn't allowed to assign no_nsfw or nsfl_access anymore.`);
       target.removeRoles(config.NSFL)
-        .catch();
+        .catch(console.log);
       target.addRole(config.noNSFW)
-        .catch();
+        .catch(console.log);
     } else {
-      message.channel.send(`\`${target.user.tag}\` is already on this blacklist`);
+      con.query(`INSERT INTO custom_roles_userage (userID, userAge, submitted, changed, allowed) VALUE ('${target.id}', 'none', '${Date.now()}', '${Date.now()}', '1')`);
+      message.channel.send(`\`${target.user.tag}\` was not in the agelist.\n They got added now with the age of 'none' and no access to the 18+ rooms.`);
       message.react('❌');
     }
   });
