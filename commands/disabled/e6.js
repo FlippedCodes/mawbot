1af09813ddd0cc53b22c9488ee2352267e394eca
Phil <philip.schaefer0@gmail.com>
2019-05-05 20:25:33 +0200

    deleted file

--- commands/disabled/e6.js
+++
@@ -1,9 +0,0 @@
-module.exports.run = async (client, message, args, con, config) => {
-  if (!message.channel.nsfw) return message.channel.send('This channel is not NSFW, please use a different channel!');
-
-  // asdf
-};
-
-module.exports.help = {
-  name: 'e6',
-};
