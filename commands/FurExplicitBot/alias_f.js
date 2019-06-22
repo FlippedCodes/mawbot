3896e6d691e1ffdff43c96ac6c32b0048e7d01c1
Phil <philip.schaefer0@gmail.com>
2019-06-22 20:56:03 +0200

    added aliases for command veration

---
+++ commands/FurExplicitBot/alias_f.js
@@ -0,0 +1,8 @@
+module.exports.run = async (client, message, args, config, functions, RichEmbed, messageOwner, fa_token_A, fa_token_B) => {
+  client.functions.get('furaffinity_core').run(client, message, args, config, functions, RichEmbed, messageOwner, fa_token_A, fa_token_B)
+    .catch(console.log);
+};
+
+module.exports.help = {
+  name: 'f',
+};
