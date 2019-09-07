2d1904a27367a9c436d5d31da08563d8af1d2bee
Phil <philip.schaefer0@gmail.com>
2019-09-07 21:47:50 +0200

    activated notificatons

--- mawbot/functions/fa_notifications.js
+++ mawbot/functions/fa_notifications.js
@@ -10,9 +10,9 @@ module.exports.run = async (client, servers, fs) => {
   let fa_token_A;
   let fa_token_B;
   if (fs.existsSync('./mawbot/config/test_token.json')) {
-    // let token = require('../config/test_token.json');
-    // fa_token_A = token.fa_cookie_a;
-    // fa_token_B = token.fa_cookie_b;
+    let token = require('../config/test_token.json');
+    fa_token_A = token.fa_cookie_a;
+    fa_token_B = token.fa_cookie_b;
   } else {
     fa_token_A = process.env.FA_COOKIE_A;
     fa_token_B = process.env.FA_COOKIE_B;
