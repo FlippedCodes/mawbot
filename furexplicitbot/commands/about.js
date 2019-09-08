9ca66dc7a5fd33e9a2e1de96ef90ba5a7307f22b
Phil <philip.schaefer0@gmail.com>
2019-09-08 18:10:29 +0200

    updated about file-link

--- furexplicitbot/commands/about.js
+++ furexplicitbot/commands/about.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 
 module.exports.run = async (client, message, args, config, RichEmbed, messageOwner, fa_token_A, fa_token_B) => {
-  fs.readFile('./config/furaffinity/about.txt', 'utf8', (err, data) => {
+  fs.readFile('./furexplicitbot/config/about.txt', 'utf8', (err, data) => {
     if (err) {
       console.log(err);
       message.react('❌');
